69489af2afae62090a899434c54cd8d89373f726
unitcodewave <unitcodewave@gmail.com>
2025-10-06 15:53:12 +0200

    Add files via upload

--- qr.js
+++ qr.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fs from 'fs';
 import pino from 'pino';
 import { makeWASocket, useMultiFileAuthState, makeCacheableSignalKeyStore, Browsers, jidNormalizedUser, fetchLatestBaileysVersion } from '@whiskeysockets/baileys';
-import { delay } from '@rexxhayanasi/elaina-bail';
+import { delay } from '@whiskeysockets/baileys';
 import QRCode from 'qrcode';
 import qrcodeTerminal from 'qrcode-terminal';
 
